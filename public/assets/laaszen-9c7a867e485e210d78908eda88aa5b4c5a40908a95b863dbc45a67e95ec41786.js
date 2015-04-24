03819340408b291c9013e8534bd7408b85fec398
Mwambaro Onkez <bahizidom@yahoo.fr>
2015-04-24 20:23:51 +0100

    LaaSzeN: assets precompiled

---
+++ public/assets/laaszen-9c7a867e485e210d78908eda88aa5b4c5a40908a95b863dbc45a67e95ec41786.js
@@ -0,0 +1,19 @@
+$(document).ready (
+    function () {
+        start_off_drop_down();
+        
+        on_click_next_prev_navs();
+        /*
+        $().click(
+            function(){
+            
+            }
+        );
+        
+        $().click(
+            function(){
+            
+            }
+        ); */  
+    }
+);
