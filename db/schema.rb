2a5e47eb35c2e43a0751631b0fa5549cb44c1843
Mwambaro Onkez <bahizidom@yahoo.fr>
2015-04-25 01:25:38 +0100

    LaaSzeN: unicorn-rails gem added for unicorn default server

--- db/schema.rb
+++ db/schema.rb
@@ -57,6 +57,18 @@ ActiveRecord::Schema.define(version: 20150424165041) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "philosophies", force: :cascade do |t|
+    t.string   "language"
+    t.string   "theme"
+    t.string   "part"
+    t.string   "chapter"
+    t.string   "topic"
+    t.string   "subtopic"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "slides", force: :cascade do |t|
     t.string   "language"
     t.string   "author"
