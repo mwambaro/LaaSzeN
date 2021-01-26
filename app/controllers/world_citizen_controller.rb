212e347bbf1d81288ebf2454e71f1003285c1603
Obed-Edom <onkezabahizi@gmail.com>
2021-01-26 13:13:49 +0200

    Home wireframe.

--- app/controllers/world_citizen_controller.rb
+++ app/controllers/world_citizen_controller.rb
@@ -3,5 +3,7 @@ class WorldCitizenController < ApplicationController
   
   def home
     @site_title_text = "World Trade System - Leadership as a Service"
+    @mission_section_card_title = "Mission"
+    @publication_section_card_title = "Publications"
   end
 end
